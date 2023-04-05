ae3b618b67d0fe8aa5e8119e380d2295a1fdea20
Ki Sar <rxs291@yahoo.com>
2023-04-05 16:25:37 -0500

    halway through the code, need to for loop now

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength = '';
+var upperCase = '';
+var lowerCase = '';
+var numbers = '';
+var specialChar = '';
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var nums = "0123456789";
+var spCHARS = "`~!@#$%^&*()_+|}{“:?><[]\\;',./-=";
+var newPassword = [];
+
+//my arrays
+var arrUPPER = alphabet.toUpperCase().split("");
+var arrLOWER = alphabet.toLowerCase().split("");
+var arrNUMS = nums.split("");
+var arrSPCH = spCHARS.split('');
 
 
 function generatePassword(){
+
+passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128");
+
+while(passwordLength<8 || passwordLength>128){
+    passwordLength = prompt("Please choose a valid length.")
+}
+
+upperCase = confirm("Would you like upper case letters?");
+lowerCase = confirm("Would you like lower case letter?");
+numbers = confirm("Would you like numbers?");
+specialChar = confirm("Would you like special characters?");
+
+while(!upperCase && !lowerCase && !numbers && !specialChar){
+    alert("You must choose at least ONE of the following criteria:");
+    upperCase = confirm("Would you like upper case letters?");
+    lowerCase = confirm("Would you like lower case letter?");
+    numbers = confirm("Would you like numbers?");
+    specialChar = confirm("Would you like special characters?");
+}
+
+if(upperCase){
+    newPassword.push(arrUPPER[Math.floor(Math.random()*arrUPPER.length)])
+}
+if(lowerCase){
+    newPassword.push(arrLOWER[Math.floor(Math.random()*arrLOWER.length)])
+}
+if(numbers){
+    newPassword.push(arrNUMS[Math.floor(Math.random()*arrNUMS.length)])
+}
+if(specialChar){
+    newPassword.push(arrSPCH[Math.floor(Math.random()*arrSPCH.length)])
+}
+
+var pass = newPassword.join("");
+console.log(newPassword);
+
+alert("You chose " + pass);
+
   // need the following variables:
   //variable for the selected length
   //array of lowercase letters
@@ -12,8 +65,8 @@ function generatePassword(){
   // use.split("") to creat arrays?
   // array to hold the password we're building
   //array to hold mega array
-  //prompt for password length --> sore in a variable
-  //validate the user input --> if user chooses <8 or > 128, they're going to need to restart or call teh cunation that prompts for length again.
+  //!!!!!!!!!!!prompt for password length --> sore in a variable
+  //!!!!!!!!!!!validate the user input --> if user chooses <8 or > 128, they're going to need to restart or call teh cunation that prompts for length again.
 
  
   // PROMPT -----> DO THEY WANT UPPER CASE LETTER?
@@ -38,6 +91,8 @@ function generatePassword(){
   // then RETURN password.
 
   console.log('YOOOOOOOOOO!');
+  var password = "COMMENT ME";
+  return pass;
 }
 // Write password to the #password input
 function writePassword() {
